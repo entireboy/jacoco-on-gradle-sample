8b5cfc8c14d924890205924973407bf56bfb5669
thDeng <entireboy@daum.net>
2020-01-13 01:49:20 +0900

    Add JaCoCo config

---
+++ src/main/java/kr/leocat/test/kotlinjacocosample/JavaFoo.java
@@ -0,0 +1,20 @@
+package kr.leocat.test.kotlinjacocosample;
+
+public class JavaFoo {
+
+    public String hello(String name) {
+        switch (name) {
+            case "펭":
+                return "하";
+            case "hello":
+                return "world";
+            default:
+                return "no one";
+        }
+    }
+
+    public void callMe() {
+        System.out.println("Please, call me");
+    }
+
+}
